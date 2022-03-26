fdc914a722c04bd836f59b2c809b0c92c4ac3045
James Pigott <jmpigott96@gmail.com>
2022-03-26 23:46:09 +0000

    Setup base app template

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="robots" content="noindex">
+        
+        @yield('meta')
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body class="font-sans antialiased">
+        <div class="min-h-screen bg-gray-100">
+            <!-- Page Content -->
+            <main id="app" class="container px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                @yield('content')
+            </main>
+        </div>
+    </body>
+</html>
