52d364e1d86e82263aa067f5fce449f330aabdd3
James Pigott <jmpigott96@gmail.com>
2022-03-28 00:47:25 +0100

    Tidy up web routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Request;
+use App\Http\Controllers\PollController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/polls/{poll}', [PollController::class, 'show'])->name('poll.show');
