fb190ec0f63190570bf51c418a1e46d8d27c37aa
James Pigott <jmpigott96@gmail.com>
2022-03-27 16:16:01 +0100

    Setup Poll -> Option relationship

--- app/Models/Option.php
+++ app/Models/Option.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Poll;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Option extends Model
 {
     use HasFactory;
+    
+    /**
+     * get the poll for this option
+     */
+    public function poll() {
+        return $this->belongsTo(Poll::class);
+    }
 }
--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Option;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Poll extends Model
 {
     use HasFactory;
+
+    /**
+     * Get the options for this poll
+     */
+    public function options() {
+        return $this->hasMany(Option::class);
+    }
 }
