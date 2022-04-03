ac6f68fe5cf2a42d62beaca90379aeda4af09b3e
James Pigott <jmpigott96@gmail.com>
2022-04-03 22:15:47 +0100

    Modal component (animations aren't working though)

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
-
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -20,6 +19,7 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('voting-form', require('./components/VotingForm.vue').default);
+Vue.component('modal', require('./components/Modal.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,6 +29,19 @@ Vue.component('voting-form', require('./components/VotingForm.vue').default);
 
 const app = new Vue({
     el: '#app',
+    data() {
+        return {
+          isModalVisible: false,
+        };
+    },
+    methods: {
+        showModal() {
+          this.isModalVisible = true;
+        },
+        closeModal() {
+          this.isModalVisible = false;
+        }
+    }
 });
 
 
---
+++ resources/js/components/Modal.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="z-100 fixed w-full h-full top-0 left-0 flex items-center justify-center transition-all duration-75 ease-in">
+        <transition
+            enter-class="ease-out duration-300"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-class="ease-in duration-200"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0">
+
+            <div class="absolute w-full h-full transition-opacity">
+                <div class="absolute inset-0 bg-gray-900 opacity-75" @click="close"></div>
+            </div>
+
+        </transition>
+
+        <transition 
+            enter-class="ease-out duration-300"
+            enter-from-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enter-to-class="opacity-100 translate-y-0 sm:scale-100"
+            leave-class="ease-in duration-200"
+            leave-from-class="opacity-100 translate-y-0 sm:scale-100"
+            leave-to-class="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+
+            <div class="bg-white w-2/3 lg:w-1/2 mx-auto rounded shadow-lg z-50 overflow-hidden transform transition-all overflow-y-scroll max-h-[80%]"
+                role="dialog"
+                aria-modal="true">
+
+                <div class="modal-content py-4 text-left px-6">
+                    <div class="flex justify-between items-center pb-3">
+                        <p class="text-2xl font-bold">
+                            <slot name="title">
+                                Modal Title
+                            </slot>
+                        </p>
+                        <div 
+                            class="modal-close hover:cursor-pointer z-50"
+                            @click="close"
+                        >
+                            <svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18">
+                                <path d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            </svg>
+                        </div>
+                    </div>
+        
+                    <!--Body-->
+                    <slot name="body">
+                        
+                    </slot>
+                </div>
+            </div>
+
+        </transition>
+        
+    </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      close() {
+        this.show = false;
+        this.$emit('close');
+      },
+    },
+  };
+</script>
