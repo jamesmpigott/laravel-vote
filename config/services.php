fcf458bdb437693b417b0309e33319a9cc08900b
James Pigott <jmpigott96@gmail.com>
2022-04-03 01:22:16 +0100

    add github to services

--- config/services.php
+++ config/services.php
@@ -33,6 +33,12 @@ return [
 
     'ipgeo' => [
         'key' => env('IP_GEOLOCATION_API_KEY'),
-    ]
+    ],
+
+    'github' => [
+        'client_id' => env('GITHUB_CLIENT_ID'),
+        'client_secret' => env('GITHUB_CLIENT_SECRET'),
+        'redirect' => env('GITHUB_REDIRECT_URL'),
+    ],
 
 ];
