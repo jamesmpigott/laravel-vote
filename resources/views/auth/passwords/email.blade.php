b8d25c59b83b3f1d7fae7cb88511a07062121754
James Pigott <jmpigott96@gmail.com>
2022-04-11 22:31:14 +0100

    Redo password reset templates

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,39 @@
 @extends('layouts.app')
 
+@section('meta')
+    <title>{{ __("Super Simple Polls") . ' | ' . __("Build beautiful polls, with none of the fuss.") }}</title>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<main id="app" class="mx-4 bg-white w-full p-8 max-w-[90%] md:max-w-[550px] lg:max-w-[40%] md:mx-auto rounded-lg shadow-md">
+    @if(session('status'))
+        <div class="mb-4 border-green-600 border-2 rounded-lg p-2.5 font-medium" role="alert">
+            {{ session('status') }}
         </div>
-    </div>
-</div>
+    @endif
+
+    <form method="POST" action="{{ route('password.email') }}" class="flex flex-col gap-y-4">
+        @csrf
+        @method('POST')
+        <div class="flex flex-col">
+            <x-label for="email">{{ __('Email Address') }}</x-label>
+
+            <x-input id="email"
+                type="email"
+                name="email"
+                value="{{ old('name') }}"
+                required
+                autocomplete="email" />
+
+            @error('email')
+                <span class="text-sm text-red-500" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        <x-button type="submit">{{ __('Send Password Reset Link') }}</x-button>
+
+    </form>
+</main>
 @endsection
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,65 +1,69 @@
 @extends('layouts.app')
 
+@section('meta')
+    <title>{{ __("Super Simple Polls") . ' | ' . __("Build beautiful polls, with none of the fuss.") }}</title>
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+<main id="app" class="mx-4 bg-white w-full p-8 max-w-[90%] md:max-w-[550px] lg:max-w-[40%] md:mx-auto rounded-lg shadow-md">
+    <form method="POST" action="{{ route('password.update') }}" class="flex flex-col gap-y-4">
+        @csrf
+        @method('POST')
+        <input type="hidden" name="token" value="{{ $token }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('password.update') }}">
-                        @csrf
+        <div class="flex flex-col">
+            <x-label for="email">{{ __('Email Address') }}</x-label>
 
-                        <input type="hidden" name="token" value="{{ $token }}">
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $email ?? old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <x-input id="email"
+                type="email"
+                name="email"
+                value="{{ $email ?? old('email') }}"
+                required
+                autocomplete="email" />
+            
+                @error('email')
+                    <span class="text-sm text-red-500" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
         </div>
-    </div>
-</div>
+
+        <div class="flex flex-col">
+            <x-label for="password">{{ __('Password') }}</x-label>
+            
+            <x-input 
+                id="password"
+                type="password"
+                name="password"
+                required 
+                autocomplete="new-password"/>
+                
+            @error('password')
+                <span class="text-sm text-red-500" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        <div class="flex flex-col">
+            <x-label for="password-confirm">{{ __('Confirm Password') }}</x-label>
+            
+            <x-input 
+                id="password-confirm"
+                type="password"
+                name="password_confirmation"
+                required 
+                autocomplete="new-password"/>
+                
+            @error('password')
+                <span class="text-sm text-red-500" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+
+        <x-button type="submit">{{ __('Reset Password') }}</x-button>
+
+    </form>
+</main>
 @endsection
