ccb24b87de0349022255a650db1cb36bf4177936
James Pigott <jmpigott96@gmail.com>
2022-04-11 21:16:34 +0100

    Tweaks to IpGeolocationService to better handle exceptions

--- app/Services/IpGeolocationService.php
+++ app/Services/IpGeolocationService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Faker;
 use Illuminate\Support\Facades\Http;
 
 class IpGeolocationService {
@@ -18,32 +19,39 @@ class IpGeolocationService {
             return $location;
         } 
 
-        $response = Http::retry(3,100)->get(
-            'https://api.ipgeolocation.io/ipgeo',
-            [
-                'apiKey' => config('services.ipgeo.key'),
-                'ip' => $ip
-            ]
-        );
+        try {
+            $response = Http::retry(3,100)->get(
+                'https://api.ipgeolocation.io/ipgeo',
+                [
+                    'apiKey' => config('services.ipgeo.key'),
+                    'ip' => $ip
+                ]
+            );
 
-        if($response->clientError() || $response->serverError()) {
-            return $location;
+            $data = json_decode($response->body());
+
+            if(json_last_error() !== JSON_ERROR_NONE) {
+                return null;
+            }
+
+            $location = [
+                'country' => $data->country_name,
+                'state' => $data->state_prov,
+                'city' => $data->city,
+                'lat' => $data->latitude,
+                'long' => $data->longitude,
+                'flag' => $data->country_flag
+            ];
+        } catch(\Exception $e) {
+            // $faker = Faker\Factory::create();
+            
+            // return [
+            //     'lat' => $faker->latitude(),
+            //     'long' => $faker->longitude()
+            // ];
+            
+            return null;
         }
-
-        $data = json_decode($response->body());
-
-        if(json_last_error() !== JSON_ERROR_NONE) {
-            return $location;
-        }
-
-        $location = [
-            'country' => $data->country_name,
-            'state' => $data->state_prov,
-            'city' => $data->city,
-            'lat' => $data->latitude,
-            'long' => $data->longitude,
-            'flag' => $data->country_flag
-        ];
 
         return $location;
     }
