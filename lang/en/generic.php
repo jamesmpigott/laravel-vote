c5b0a15a5721a341f422e71cfe1cec1de12c44f2
James Pigott <jmpigott96@gmail.com>
2022-04-14 22:12:34 +0100

    Additional i18n files

---
+++ lang/en/generic.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    'greeting' => "Hi, :name",
+    'no-polls' => "You haven't built any polls yet! Click the button above to change that...",
+    'your-polls' => "Your Polls",
+    'choice' => "Choice",
+    'votes' => "Votes",
+    'email' => "Email Address",
+    'submit' => "Submit",
+    'vote-submitted' => "Thank you for your vote!"
+];
---
+++ lang/en/poll-builder.php
@@ -0,0 +1,12 @@
+<?php 
+
+return [
+    'title' => "Poll Builder",
+    'question' => "What's your question?",
+    'choices' => "What are the choices?",
+    'geolocation' => "Vote Geolocation Tracking",
+    'anon_voting' => "Anonymous Voting",
+    'create' => "Create your poll",
+    'created' => "Your Poll has been created!",
+    'build-poll' => "Build a new Poll"
+];
